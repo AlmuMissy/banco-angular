650e796b487b9565df8eee82697ee96f25c29626
Almudena <almufullstack@gmail.com>
2022-06-30 20:25:28 +0200

    yejejei

--- src/app/services/banco.service.ts
+++ src/app/services/banco.service.ts
@@ -1,14 +1,17 @@
-import { Injectable, Type } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Respuesta } from '../modelos/respuesta';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BancoService {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
-  async loginGestor(usuario: string, password: string):Promise<boolean> {
+  async loginGestor(
+    usuario: string,
+    password: string): Promise<boolean> {
 
     const response = await fetch(
       'http://127.0.0.1:8085/login/gestor/', {
@@ -20,8 +23,35 @@ export class BancoService {
       }
     );
 
-    const datos:Respuesta = await response.json();
-    return datos.ok;
+    const datos: Respuesta = await response.json();
 
+    // si la autenticación ha sido exitosa, invocamos al método autenticado del servicio auth
+    if(datos.ok) {
+      this.authService.autenticado(datos.data.token, usuario,'gestor' )
+    }
+
+    return datos.ok;
   }
+
+  async loginCliente(
+    usuario: string,
+    password: string): Promise<boolean> {
+
+    const response = await fetch(
+      'http://127.0.0.1:8085/login/cliente/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: `usuario=${usuario}&password=${password}`
+      }
+    );
+
+    const datos: Respuesta = await response.json();
+    if(datos.ok) {
+      this.authService.autenticado(datos.data.token, usuario,'cliente' )
+    }
+    return datos.ok;
+  }
+
 }
